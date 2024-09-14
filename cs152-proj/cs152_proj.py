da3adc6fb10a0b44865439f000bedef679891914
jessimoo <fung.jessica03@gmail.com>
2024-09-13 18:31:26 -0700

    third test

--- cs152-proj/cs152_proj.py
+++ cs152-proj/cs152_proj.py
@@ -1,3 +1,6 @@
+#references: 
+#https://www.pythonguis.com/tutorials/create-gui-tkinter/
+
 from tkinter import* #python GUI
 window = Tk() #create window (root) 
 
@@ -5,5 +8,8 @@ window.title("Grocery To-Do List") #changes window title
 window.minsize(400,400)
 window.configure(background="pink")
 
-#leave here! 
+text = Label(window, text="Homepage")
+text.pack()
+
+#leave at bottom! 
 window.mainloop() 
