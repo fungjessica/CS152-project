98f177d13174cf7ccf1b66c351ca58f63f6cf939
Swan Pyae Sone Tun <swanpyae.steven26@gmail.com>
2024-10-03 21:22:45 -0700

    test

--- cs152-proj/cs152_proj.py
+++ cs152-proj/cs152_proj.py
@@ -1,16 +1,61 @@
 #references: 
 #https://www.pythonguis.com/tutorials/create-gui-tkinter/
 
-from tkinter import* #python GUI
-window = Tk() #create window (root) 
+import tkinter as tk
+from tkinter import messagebox
 
-window.title("Grocery To-Do List") #changes window title
-window.minsize(400,400)
-window.configure(background="pink")
+class TodoApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("To-Do List")
+        self.root.geometry("300x400")
 
-text = Label(window, text="Homepage")
-text.pack()
-button = Button(window, text="button")
-button.pack(side='top')
-#leave at bottom! 
-window.mainloop() 
+        self.tasks = []
+
+        # Label
+        self.title_label = tk.Label(self.root, text="To-Do List", font=("Helvetica", 16))
+        self.title_label.pack(pady=10)
+
+        # Task Entry
+        self.task_entry = tk.Entry(self.root, width=25)
+        self.task_entry.pack(pady=10)
+
+        # Add Task Button
+        self.add_task_button = tk.Button(self.root, text="Add Task", command=self.add_task)
+        self.add_task_button.pack(pady=5)
+
+        # Listbox to display tasks
+        self.task_listbox = tk.Listbox(self.root, width=40, height=10)
+        self.task_listbox.pack(pady=10)
+
+        # Remove Task Button
+        self.remove_task_button = tk.Button(self.root, text="Remove Task", command=self.remove_task)
+        self.remove_task_button.pack(pady=5)
+
+    def add_task(self):
+        task = self.task_entry.get()
+        if task != "":
+            self.tasks.append(task)
+            self.update_task_listbox()
+            self.task_entry.delete(0, tk.END)
+        else:
+            messagebox.showwarning("Input Error", "You must enter a task!")
+
+    def remove_task(self):
+        try:
+            selected_task_index = self.task_listbox.curselection()[0]
+            self.tasks.pop(selected_task_index)
+            self.update_task_listbox()
+        except IndexError:
+            messagebox.showwarning("Selection Error", "You must select a task to remove!")
+
+    def update_task_listbox(self):
+        self.task_listbox.delete(0, tk.END)
+        for task in self.tasks:
+            self.task_listbox.insert(tk.END, task)
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = TodoApp(root)
+    root.mainloop()
