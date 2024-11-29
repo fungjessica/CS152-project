57fc379c352bd1dd76cf6a7a9c3ba023ab7ecc9a
Swan Pyae Sone Tun <swanpyae.steven26@gmail.com>
2024-11-28 20:30:34 -0800

    changed how browsing groceries works

--- cs152-proj/src/clist.py
+++ cs152-proj/src/clist.py
@@ -195,7 +195,7 @@ def choose_items_flow(root, controller, list_name):
     back_button.pack(pady=10)
 
     # Function to proceed to quantity selection
-     proceed_to_quantities():
+    def proceed_to_quantities():
         if selected_items:
             selected_items_dict = {item: {"selected": True, "quantity": None} for item in selected_items}
             set_quantities_flow(root, controller, list_name, selected_items_dict)
